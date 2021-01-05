5475d59464c8b172b8632e61ccae920ce2127afc
shelleyHLX <2501967525@qq.com>
2021-01-05 17:30:00 +0800

    first

---
+++ autograd/build/CMakeFiles/autograd.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/autograd.dir/autograd.cpp.o"
+  "autograd.pdb"
+  "autograd"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/autograd.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
